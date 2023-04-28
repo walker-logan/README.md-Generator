ca92b0b5258970ad8ec4c3ef0c71b3b85cb766b2
Walker Logan <walker.logan44@gmail.com>
2023-04-28 15:41:35 -0500

    added question list pseudocode and defined some variables

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,22 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
+
+// Questions:
+// Title?
+// Want to have a table of contents? y/n
+// Description of the project?
+// Would you like to include a live link to the site, if it exists? y/n
+// How does the user install your project?
+// How does one use the project?
+// Who/what all do you wan't to give credit?
+// What license does your project use, if it exists?
+// How can users contribute to the project?
+// How can users test the project?
+
 const questions = [];
 
 // TODO: Create a function to write README file
